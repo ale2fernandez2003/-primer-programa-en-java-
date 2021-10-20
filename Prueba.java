51cf7a4d859908348688f0ef3f85e55bcef1652f
ale2fernandez2003 <92783863+ale2fernandez2003@users.noreply.github.com>
2021-10-20 12:50:43 +0200

    Pude poner donde vivo y el ordenador que uso

--- Prueba.java
+++ Prueba.java
@@ -2,6 +2,7 @@ class Prueba {
   public static void main(String [] args){
 	System.out.println("Alejandro Fernandez Fernandez");
 	System.out.println("Naci en leon en 2003");
+	System.out.println("Vivo en leon y uso un ordenador thosiba");
 
   }
 }
