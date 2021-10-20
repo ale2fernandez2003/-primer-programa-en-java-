2c724e203933fe834c10367fdd83061a417ad952
ale2fernandez2003 <92783863+ale2fernandez2003@users.noreply.github.com>
2021-10-20 13:28:43 +0200

    He cambiado el nombre de lugar y puse a mi cantante favorito

--- Prueba.java
+++ Prueba.java
@@ -1,8 +1,9 @@
 class Prueba {
   public static void main(String [] args){
-	System.out.println("Alejandro Fernandez Fernandez");
 	System.out.println("Naci en leon en 2003");
 	System.out.println("Vivo en leon y uso un ordenador thosiba");
+	System.out.println("Alejandro Fernandez Fernandez");
+	System.out.println("Bad Bunny");
 
   }
 }
